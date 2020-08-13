22a297ea322c115b4cbda7e4da5489773e33de9c
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-08-12 22:28:26 -0300

    maxidoctor - footer alterado..

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: end;
 
     width: 100%;
     max-width: 442px;
