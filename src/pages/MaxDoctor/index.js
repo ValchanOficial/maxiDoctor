811ce0d9553c1c3ca7235c4da68860575d44204e
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2020-08-13 17:54:49 -0300

    clean

--- src/pages/MaxDoctor/index.js
+++ src/pages/MaxDoctor/index.js
@@ -5,13 +5,11 @@ import Api from '../../services/api';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Form from '../../components/Form';
-// import Message from '../../components/Message';
 
 import { Container } from './styles';
 
 const MaxDoctor = () => {
     const [list, setList] = useState([]);
-    // const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
         Api.getStateList()
@@ -23,7 +21,6 @@ const MaxDoctor = () => {
         <Container>
             <Header />
             <Form list={list}/>
-            {/* <Message isVisible={isVisible} /> */}
             <Footer />
         </Container>
     );
